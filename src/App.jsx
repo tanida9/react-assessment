93472a525a34cd6e41e6e35de5add890ed2b491f
tanida <tanida.manee@gmail.com>
2025-04-02 11:21:18 +0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 export default function App() {
+  
   return (
     <>
       <h1 className="text-blue-400 font-extrabold">Hello, World!</h1>
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,10 @@
-//import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
-// React Component
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  //<StrictMode>
-    <App />
-  //</StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={} />
 )
