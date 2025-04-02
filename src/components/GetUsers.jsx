74d26fc72a0614a4bdc24505eecd2b1db29e88a9
tanida <tanida.manee@gmail.com>
2025-04-02 21:56:01 +0700

    feat(file): Updated link in Owner.jsx

--- src/components/GetUsers.jsx
+++ src/components/GetUsers.jsx
@@ -8,8 +8,8 @@ const GetUsers = () => {
 
   useEffect(() => {
     const fetchUsers = async () => {
-      //setError("");
-      //setLoading(true);
+      setError("");
+      setLoading(true);
 
       try {
         const res = await axios.get(
@@ -59,7 +59,7 @@ const GetUsers = () => {
                 <td className="border border-gray-300 py-2 px-4">{user.position}</td>
                 <td className="border border-gray-300 py-2 px-4">
                   <button type="submit" className="bg-red-600 text-white px-4 py-1 rounded hover:bg-red-700">
-                    Delete User
+                    Delete
                   </button>
                 </td>
               </tr>
